f5c1a909e0451ec91f7314b90fb0ea1abf2286a9
Noah <noaheasley408@gmail.com>
2021-03-29 16:55:56 -0500

    Added moving pipes and flapping bird

---
+++ Assets/Scripts/Movement/MovementBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour
+{
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Speed,0,0) * Time.deltaTime;
+    }
+}
